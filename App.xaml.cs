a5264265389711f98da53e8836bce0cbfbc2c34f
Paweł Sierzputowski <sierzput@gmail.com>
2017-09-21 20:57:16 +0200

    Change assembly name

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Configuration;
-using System.Data;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace Projekt2
